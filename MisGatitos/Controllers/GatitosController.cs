e5282b0e41e3c306d0a9aa2ed1772a0d62cb0557
J. Manuel Almendarez <jmanuel.9@hotmail.com>
2024-09-07 18:58:34 -0600

    Creación de métodos a utilizar

--- MisGatitos/Controllers/GatitosController.cs
+++ MisGatitos/Controllers/GatitosController.cs
@@ -5,25 +5,29 @@ using System;
 using System.Collections.Generic;
 using MisGatitos.Models;
 using Newtonsoft.Json;
+using System.Configuration;
 
 namespace MisGatitos.Controllers
 {
     public class GatitosController : Controller
     {
         private readonly HttpClient _client;
+        private readonly string _uriApi;
 
         public GatitosController()
         {
             _client = new HttpClient();
             _client.DefaultRequestHeaders.Add("x-api-key", "live_Px4R2oxR6FRVibSRNV9SOXSkcYbpJpOj5sayes4j48KnZtu5t5aEExMXuOYIhcE6");
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _client.BaseAddress = new Uri("https://api.thecatapi.com/");
+            _uriApi = ConfigurationManager.AppSettings["CatApi"];
+            _client.BaseAddress = new Uri(_uriApi);
         }
 
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(string CatBreeds, int? limit)
         {
             IEnumerable<Gato> gatos = GetGatos();
+            IEnumerable<Resultado> resultados = new List<Resultado>();
 
             ViewBag.CatBreeds = new SelectList(gatos, "id", "name");
 
@@ -43,6 +47,20 @@ namespace MisGatitos.Controllers
             }
 
             return gatos;
+        }
+
+        public IEnumerable<Resultado> GetResultados(string catBreeds, int? limit)
+        {
+            IEnumerable<Resultado> resultados = new List<Resultado>();
+            var response = _client.GetAsync($"v1/images/search?breed_ids={catBreeds}&limit={limit}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                resultados = JsonConvert.DeserializeObject<IEnumerable<Resultado>>(responseContent);
+            }
+
+            return resultados;
         }
 
 
