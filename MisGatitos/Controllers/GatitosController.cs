47bce1e7243bee4abe2443dd87d2d24e1c7f2a8a
J. Manuel Almendarez <jmanuel.9@hotmail.com>
2024-09-07 18:00:48 -0600

    Creacion de modelos y base de API

---
+++ MisGatitos/Controllers/GatitosController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System;
+
+namespace MisGatitos.Controllers
+{
+    public class GatitosController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public GatitosController()
+        {
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("x-api-key", "live_Px4R2oxR6FRVibSRNV9SOXSkcYbpJpOj5sayes4j48KnZtu5t5aEExMXuOYIhcE6");
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri("https://api.thecatapi.com/");
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ MisGatitos/Models/Resultado.cs
@@ -0,0 +1,11 @@
+﻿namespace MisGatitos.Models
+{
+    public class Resultado
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+    }
+}
